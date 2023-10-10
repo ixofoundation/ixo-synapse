c490fab1e82aaf1608041201872ae5e2321c2385
Michael Pretorius <michaelpretorius96@gmail.com>
2023-10-10 10:34:01 +0200

    test logging

--- synapse_ixo/ixo/module.py
+++ synapse_ixo/ixo/module.py
@@ -1,8 +1,11 @@
+import logging
 from typing import Literal, Union
 import synapse as synapse
 import synapse.module_api as module_api
 import synapse.api.errors as errors
 import synapse.events as events
+
+logger = logging.getLogger("synapse.contrib." + __name__)
 
 
 # New module API
@@ -22,7 +25,10 @@ class Module:
     async def check_event_for_spam(
         self, event: "events.EventBase"
     ) -> Union[Literal["NOT_SPAM"], errors.Codes]:
+        logger.info("check_event_for_spam!!!!!!!!!!!!!!!!!")
         if event.type in self.block_events:
+            logger.info("block !!!!!!!!!!!!!!!!!")
             return errors.Codes.FORBIDDEN
         else:
+            logger.info("no block !!!!!!!!!!!!!!!!!")
             return errors.NOT_SPAM
